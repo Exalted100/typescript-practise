da252c269903cdefa4042ec9bd0630ccaea711c8
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-10 08:06:58 +0100

    commit

--- hello.js
+++ hello.js
@@ -3,3 +3,23 @@ function greet(person, date) {
     console.log("Hello " + person + ", today is " + date.toDateString() + "!");
 }
 greet("Brendan", new Date());
+function printName(obj) {
+    console.log("first name is " + obj.first + (obj.last ? "last name is " + obj.last : ""));
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+//UNION TYPE
+function printId(id) {
+    console.log("Your ID is: " + id);
+}
+// Exactly the same as the earlier example
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+//LITERAL TYPES
+function printText(s, alignment) {
+    // ...
+}
+printText("Hello, world", "left");
--- hello.ts
+++ hello.ts
@@ -5,3 +5,33 @@ function greet(person: string, date: Date) {
 }
   
 greet("Brendan", new Date()); 
+
+function printName(obj: { first: string; last?: string }) {
+    console.log(`first name is ${obj.first}${obj.last ? `last name is ${obj.last}` : ""}`)
+  }
+  // Both OK
+  printName({ first: "Bob" });
+  printName({ first: "Alice", last: "Alisson" });
+
+//UNION TYPE
+function printId(id: number | string) {
+    console.log("Your ID is: " + id);
+  }
+
+//TYPE ALIAS
+type Point = {
+    x: number;
+    y: number;
+  };
+  
+  // Exactly the same as the earlier example
+  function printCoord(pt: Point) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+  }
+
+//LITERAL TYPES
+function printText(s: string, alignment: "left" | "right" | "center") {
+    // ...
+  }
+  printText("Hello, world", "left");
